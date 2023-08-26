bdeadf5dab11db662bb52e19bb3731381bec9fb7
Max Shvarts <maxims33@gmail.com>
2023-08-26 17:34:25 +1000

    Fixing pylint warnings

--- speakeasy/customagents.py
+++ speakeasy/customagents.py
@@ -1,3 +1,9 @@
+"""
+Module defining custom langchain agents
+#TODO Exploring other agents and output parsers E.g.: Structured/Pydantic Chat? OpenAI functions?
+#TODO Exploring adding memory backed by a Vectorstore. Explicit memory 'management'
+"""
+
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 from langchain.agents import initialize_agent, AgentExecutor
 from langchain.schema import AgentAction, AgentFinish
@@ -10,16 +16,14 @@ from .tools.custom_tools import CustomInstructLLMTool, CustomMathTool, RunCodeTo
 from .tools.vectorstore_tools import CustomDocumentQueryTool, CustomImageQueryTool
 from .tools.image_tools import CustomGenerateImageTool, SearchImageTool
 
-#TODO Exploring other agents and output parsers E.g.: Structured/Pydantic Chat? OpenAI functions?
-#TODO Exploring adding memory backed by a Vectorstore. Explicit memory 'management' 
-
 class CustomBaseAgent(object):
+    """ Base class for customer agents """
     def _handle_agent_error(self, error) -> str:
         print("THE OUTPUT FORMAT IS INCORRECT!")
-        return f"CHECK YOUR OUTPUT FORMAT! {str(error)[:200]}"
+        returnlass f f"CHECK YOUR OUTPUT FORMAT! {str(error)[:200]}"
 
-# Use only with Bard 
 class CustomBardAgent(CustomBaseAgent):
+    """ Use only with Bard """
     @property
     def observation_prefix(self) -> str:
         """Prefix to append the observation with."""
@@ -51,7 +55,8 @@ class CustomBardAgent(CustomBaseAgent):
                 )
             # Adding to the previous steps, we now tell the LLM to make a final pred
             thoughts += (
-                    "\n\nI now need to return ONLY a final answer based on the previous steps. The answer MUST be prefixed with the following exact label 'Final Anwer: '. DO NOT OUTPUT ACTION TAG!"
+                "\n\nI now need to return ONLY a final answer based on the previous steps. "
+                "The answer MUST be prefixed with the following label 'Final Anwer: '. DO NOT OUTPUT ACTION TAG!"
             )
             new_inputs = {"agent_scratchpad": thoughts, "stop":None} #Setting stop to None
             full_inputs = {**kwargs, **new_inputs}
@@ -72,18 +77,26 @@ class CustomBardAgent(CustomBaseAgent):
             )
 
 class CustomSingleActionAgent(CustomBaseAgent, ZeroShotAgent):
+    """ Custom single action agents """
     def __init__(self, llm_chain, allowed_tools, verbose):
-        super(CustomSingleActionAgent, self).__init__(llm_chain=llm_chain, allowed_tools=allowed_tools, verbose=verbose)
+        super(CustomSingleActionAgent, self).__init__(llm_chain=llm_chain,
+                allowed_tools=allowed_tools,
+                verbose=verbose
+        )
 
 class CustomConversationalAgent(CustomBaseAgent, ConversationalAgent):
+    """ Custom conversation agent """
     def __init__(self, llm_chain, allowed_tools, verbose):
-        super(CustomConversationalAgent, self).__init__(llm_chain=llm_chain, allowed_tools=allowed_tools, verbose=verbose)
+        super(CustomConversationalAgent, self).__init__(llm_chain=llm_chain,
+            allowed_tools=allowed_tools,
+            verbose=verbose
+        )
 
 
 # ------------------------------------------------------------------------------------
 
-# Some issues handling documents with sources - reverting to custom tools
 def base_tools(factory, doc_db, img_db) -> List:
+    """ Some issues handling documents with sources - reverting to custom tools """
     vectorstore_info = VectorStoreInfo(
         name="Local_Documents",
         description="Documents and PDFs",
@@ -103,27 +116,35 @@ def base_tools(factory, doc_db, img_db) -> List:
     return bt
 
 def custom_tools(factory, doc_db, img_db, include_base_tools = False) -> List:
+    """ Collect the custom tools, and base tools if desired """
     bt = []
     if include_base_tools == True:
         bt = base_tools(factory, doc_db, img_db)
-    
+
     ct = [
         CustomInstructLLMTool(factory),
         CustomMathTool(factory),
         CustomGenerateImageTool(factory, 
-            return_direct=True, 
-            api_url=factory.env_config['sd_url'], 
+            return_direct=True,
+            api_url=factory.env_config['sd_url'],
             generation_steps=factory.env_config['sd_steps'],
             output_filename=factory.env_config['image_output_filename']
         ),
-        CustomDocumentQueryTool(factory, doc_db), 
-        CustomImageQueryTool(factory, img_db), 
+        CustomDocumentQueryTool(factory, doc_db),
+        CustomImageQueryTool(factory, img_db),
         SearchImageTool(factory, return_direct=True),
         RunCodeTool(factory, return_direct=True)
     ]
     return ct + bt
 
-def default_agent_executor(agent, tools, memory=None, max_iterations=1, verbose=True, early_stopping_method='generate'):
+def default_agent_executor(agent,
+        tools,
+        memory=None,
+        max_iterations=1,
+        verbose=True,
+        early_stopping_method='generate'
+    ):
+    """ Create a deafult agent executor """
     return AgentExecutor.from_agent_and_tools(
         agent=agent,
         memory=memory,
@@ -134,8 +155,8 @@ def default_agent_executor(agent, tools, memory=None, max_iterations=1, verbose=
         handle_parsing_errors=agent._handle_agent_error
     )
 
-# Initialize Agent - Without memory
 def init_agent(factory, doc_db, img_db, max_iterations=1, verbose=True, early_stopping_method='generate'):
+    """ Initialize Agent - Without memory """
     prefix = """Answer the following questions or tasks as best you can. Ensure to choose one of the following tools as the 'Action':"""
     suffix = """You MUST to stick to the format provided above, but do not output the 'Final Answer:' until instructed. Begin!\n
 Question: {input}
@@ -145,15 +166,15 @@ Question: {input}
     prompt = CustomSingleActionAgent.create_prompt(tools, input_variables=["input", "agent_scratchpad"]) #prefix=prefix, suffix=suffix,
     llm_chain = LLMChain(llm=factory.llm, prompt=prompt)
     agent = CustomSingleActionAgent(llm_chain=llm_chain, allowed_tools=tool_names, verbose=verbose)
-    agent_executor = default_agent_executor(agent, tools, 
-            max_iterations=max_iterations, 
-            verbose=verbose, 
+    agent_executor = default_agent_executor(agent, tools,
+            max_iterations=max_iterations,
+            verbose=verbose,
             early_stopping_method=early_stopping_method
-        ) 
+        )
     return agent_executor
 
-# Initialize Conversational Agent - With memory
-def init_conversational_agent(factory, doc_db, img_db, max_iterations=1, verbose=True, early_stopping_method='generate'): 
+def init_conversational_agent(factory, doc_db, img_db, max_iterations=1, verbose=True, early_stopping_method='generate'):
+    """ Initialize Conversational Agent - With memory """
     prefix = """You are Assistant, a large language model. Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
 Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
@@ -188,15 +209,19 @@ Thought: Do I need to use a tool? No
 ```"""
     tools = custom_tools(factory, doc_db, img_db)
     tool_names = [tool.name for tool in tools]
-    prompt = CustomConversationalAgent.create_prompt(tools, prefix=prefix, suffix=suffix, format_instructions=instruction_format, input_variables=["chat_history", "input", "agent_scratchpad"]) 
+    prompt = CustomConversationalAgent.create_prompt(tools,
+        prefix=prefix,
+        suffix=suffix,
+        format_instructions=instruction_format,
+        input_variables=["chat_history", "input", "agent_scratchpad"]
+    ) 
     llm_chain = LLMChain(llm=factory.llm, prompt=prompt)
     agent = CustomConversationalAgent(llm_chain=llm_chain, allowed_tools=tool_names, verbose=verbose)
     mem = memory=ConversationBufferMemory(memory_key="chat_history")
-    agent_executor = default_agent_executor(agent, tools, 
-            memory=mem, 
-            max_iterations=max_iterations, 
-            verbose=verbose, 
+    agent_executor = default_agent_executor(agent, tools,
+            memory=mem,
+            max_iterations=max_iterations,
+            verbose=verbose,
             early_stopping_method=early_stopping_method
-        ) 
+        )
     return agent_executor
-
