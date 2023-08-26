0488a4405a4b28d7a91c34be47d7b439e977f8c1
Max Shvarts <maxims33@gmail.com>
2023-08-26 16:08:01 +1000

    fixing pylint warnings

--- speakeasy/customloaders.py
+++ speakeasy/customloaders.py
@@ -24,15 +24,15 @@ class CustomCaptionLoader(BaseLoader): #pylint: disable=too-few-public-methods
         Load from a list of image files. Assumes only single item in list I think
         """
         try:
-            from clip_interrogator import Config, Interrogator
-        except ImportError:
+            from clip_interrogator import Config, Interrogator #pylint: disable=import-outside-toplevel
+        except ImportError as exec:
             raise ValueError(
                 "Raising Exception. Missing clip_interrogator moddule"
-            )
+            ) from exec
 
-        config = Config(clip_model_name="ViT-L-14/openai", 
-            blip_model_type='blip-large', 
-            device='cuda', 
+        config = Config(clip_model_name="ViT-L-14/openai",
+            blip_model_type='blip-large',
+            device='cuda',
             cache_path="./img_embeddings"
         )
         config.apply_low_vram_defaults()
@@ -40,12 +40,12 @@ class CustomCaptionLoader(BaseLoader): #pylint: disable=too-few-public-methods
         doc_list : List[Document] = []
         for it in self.image_paths: #pylint: disable=invalid-name
             image = Image.open(it).convert('RGB')
-            #caption = cap_int.interrogate(image) # Reduced memory required by not running clip model
+            #caption = cap_int.interrogate(image) # Reduced memory needed by not running clip model
             caption = cap_int.generate_caption(image)
             print(f"Image Caption for {it}: {caption}")
             doc = Document(page_content=caption, metadata={"source": str(it)})
             doc_list.append(doc)
-        return docc_list
+        return doc_list
 
 class DirectoryCaptionLoader(DirectoryLoader): #pylint: disable=too-few-public-methods
     """ Load directoy of captions of images """
@@ -54,10 +54,12 @@ class DirectoryCaptionLoader(DirectoryLoader): #pylint: disable=too-few-public-m
         self.image_paths = []
 
     def load_file(self, item, path, docs, pbar): #pylint: disable=unused-argument
+        """ override the load_files method """
         if item.is_file():
             self.image_paths.append(item)
-    
+
     def load(self) -> List[Document]:
+        """ override the load method """
         super().load()
         captions = CustomCaptionLoader(self.image_paths).load()
         return captions
--- speakeasy/indexes.py
+++ speakeasy/indexes.py
@@ -1,20 +1,22 @@
+"""
+Module for working with vectorstores
+"""
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
-from .customloaders import CustomCaptionLoader, DirectoryCaptionLoader
 from langchain.document_loaders import (
-        DirectoryLoader, 
-        TextLoader, 
+        DirectoryLoader,
+        TextLoader,
         CSVLoader,
-        PyPDFLoader,
         UnstructuredPDFLoader,
         UnstructuredHTMLLoader,
         UnstructuredPowerPointLoader,
-        UnstructuredWordDocumentLoader,
-        ImageCaptionLoader
+        UnstructuredWordDocumentLoader
 )
-
+from .customloaders import DirectoryCaptionLoader
 
 def load_document_helper(folder_path, file_ext, loader_class, text_splitter = None):
+    """ helper method for loading text documents """
     loader = DirectoryLoader(folder_path, glob=file_ext, loader_cls=loader_class)
     docs = loader.load()
     if text_splitter == None:
@@ -24,44 +26,50 @@ def load_document_helper(folder_path, file_ext, loader_class, text_splitter = No
     print(f"Number of {file_ext}: {len(docs)}, Number of texts split: {len(texts)}")
     return texts
 
-# Initialize the Document VectorStore
 def init_document_db(factory, file_path, persist_dir, chunk_size = 1500, chunk_overlap = 200):
+    """ Initialize the Document VectorStore """
     # Create Text Splitter
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = chunk_size,
         chunk_overlap  = chunk_overlap
     )
-    texts = load_document_helper(file_path, "./*.pdf", UnstructuredPDFLoader, text_splitter) #PydPDFLoader loads a doc per page
-    texts += load_document_helper(file_path, "./*.html", UnstructuredHTMLLoader, text_splitter) 
-    texts += load_document_helper(file_path, "./*.pptx", UnstructuredPowerPointLoader, text_splitter) 
-    texts += load_document_helper(file_path, "./*.docx", UnstructuredWordDocumentLoader, text_splitter) 
-    texts += load_document_helper(file_path, "./*.csv", CSVLoader, text_splitter)
-    texts += load_document_helper(file_path, "./*.txt", TextLoader, text_splitter)
+    texts = load_document_helper(file_path, "./*.pdf", 
+        UnstructuredPDFLoader, text_splitter) 
+    #PydPDFLoader loads a doc per page
+    texts += load_document_helper(file_path, "./*.html", 
+        UnstructuredHTMLLoader, text_splitter)
+    texts += load_document_helper(file_path, "./*.pptx", 
+        UnstructuredPowerPointLoader, text_splitter)
+    texts += load_document_helper(file_path, "./*.docx", 
+        UnstructuredWordDocumentLoader, text_splitter)
+    texts += load_document_helper(file_path, "./*.csv", 
+        CSVLoader, text_splitter)
+    texts += load_document_helper(file_path, "./*.txt", 
+        TextLoader, text_splitter)
     return create_document_db(factory, texts, persist_dir = persist_dir)
 
 def create_document_db(factory, texts, persist_dir):
-    # Create the VectorStore DB
+    """ Create the VectorStore DB """
     if len(texts) > 0:
         return Chroma.from_documents(texts, factory.embeddings, persist_directory=persist_dir)
 
 def load_document_db(factory, persist_dir):
-    # Load the VectorStore DB
-    db = Chroma(persist_directory=persist_dir, embedding_function=factory.embeddings)    
-    return db
+    """ Load the VectorStore DB """
+    vdb = Chroma(persist_directory=persist_dir, embedding_function=factory.embeddings)
+    return vdb
 
-# Initialize the Image VectorStore
 def init_image_db(factory,  file_path, persist_dir):
+    """ Initialize the Image VectorStore """
     docs = DirectoryCaptionLoader(file_path = file_path, glob = "./*.jpg").load()
     docs += DirectoryCaptionLoader(file_path = file_path, glob = "./*.png").load()
     return create_image_db(factory, docs, persist_dir = persist_dir)
 
 def create_image_db(factory, docs, persist_dir):
-    # Create the VectorStore DB
+    """ Create the VectorStore DB """
     if len(docs) > 0:
         return Chroma.from_documents(docs, factory.embeddings, persist_directory=persist_dir)
 
 def load_image_db(factory, persist_dir):
-    # Load the VectorStore DB
-    db = Chroma(persist_directory=persist_dir, embedding_function=factory.embeddings)
-    return db
-
+    """ Load the VectorStore DB """
+    vdb = Chroma(persist_directory=persist_dir, embedding_function=factory.embeddings)
+    return vdb
